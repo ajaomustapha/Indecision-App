464c8a019b462bc8612b0edc8b69bfe5682928ea
ajaomustapha <connectajao@gmail.com>
2020-06-11 14:41:04 +0100

    Learnt about classes and subclass

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,8 +13,43 @@ class Person {
     }
 }
 
-const me = new Person('Mustapha Ajao', 78);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+        if(this.hasMajor()) {
+            description +=  ` Their major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.homeLocation) {
+            greeting += ` I am visiting from ${this.homeLocation}.`
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler('Mustapha Ajao', 78, 'Oklahoma');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
